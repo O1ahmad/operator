4be2bf37eba6df5211c8992cbf810aaabfa665ca
Ahmad Wilson <zer0ne.io.x@gmail.com>
2021-11-03 21:57:04 -0400

    refactor Dockerfile and add /key API to upload ssh keys

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:slim-buster
+
+ARG debug_mode=development
+
+ENV FLASK_APP=/src/app
+ENV FLASK_ENV=$debug_mode
+ENV ROLES_DIR=/roles
+
+RUN DEBIAN_FRONTEND=noninteractive apt-get update && \
+    apt install -y python3-pip sshpass git openssh-client libhdf5-dev libssl-dev libffi-dev && \
+    rm -rf /var/lib/apt/lists/* && apt-get clean
+
+WORKDIR /src
+ADD src/requirements.txt /src/requirements.txt
+RUN pip install -r /src/requirements.txt
+
+COPY src /src
+COPY ansible/O1labs/crypto/roles /roles
+
+#         web
+#          ↓
+EXPOSE 1001/tcp
+
+CMD ["flask", "run", "--host=0.0.0.0", "--port=1001"]
--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
+import re
 import ansible_runner
 from flask import Flask, request
+import os
 import sys
 import yaml
 
 app = Flask(__name__)
 
 
+KEYS_DIR = "/keys"
 WORK_DIR = "/var/tmp/"
-ROLES_DIR = "{}/roles".format(WORK_DIR)
+ROLES_DIR = os.environ.get("ROLES_DIR", "{}/roles".format(WORK_DIR))
 
 CONSTRUCTS = {}
 
@@ -46,6 +49,22 @@ def generate_inventory(spec):
 
     return spec['id']
 
+@app.route("/key", methods=['GET', 'POST'])
+def key():
+    if 'ssh' in request.files:
+        file = request.files['ssh']
+        path = "{}/{}".format(KEYS_DIR, file.filename)
+
+        if not os.path.exists(KEYS_DIR):
+            os.makedirs(KEYS_DIR)
+
+        file.save(path)
+        os.chmod(path, 0o600)
+
+        return "Successfully uploaded key!"
+    
+    return "No operation performed."
+
 @app.route("/construct", methods=['GET', 'POST'])
 def construct():
     error = None
@@ -54,15 +73,21 @@ def construct():
         generate_inventory(data)
         generate_setup(data)
 
+        cmd_args = [
+            '{dir}/{id}.yaml'.format(dir=WORK_DIR, id=data['id']),
+            '-i',
+            '{dir}/inventory-{id}.yaml'.format(dir=WORK_DIR, id=data['id']),
+            '-v'
+        ]
+
+        if 'ssh_key' in request.args and os.path.exists("/{}/{}".format(KEYS_DIR, request.args['ssh_key'])):
+            cmd_args.extend(["--private-key", "/{}/{}".format(KEYS_DIR, request.args['ssh_key'])])
+        else:
+            cmd_args.append("--ask-pass")
+
         out, err, rc = ansible_runner.run_command(
             executable_cmd='ansible-playbook',
-            cmdline_args=[
-                '{dir}/{id}.yaml'.format(dir=WORK_DIR, id=data['id']),
-                '-i',
-                '{dir}/inventory-{id}.yaml'.format(dir=WORK_DIR, id=data['id']),
-                '-vvvv',
-                '-k'
-            ],
+            cmdline_args=cmd_args,
             input_fd=sys.stdin,
             output_fd=sys.stdout,
             error_fd=sys.stderr,
@@ -70,9 +95,9 @@ def construct():
 
         CONSTRUCTS[data['id']] = data['setup']
 
-        return "out={}, err={}, rc={}".format(out, err, rc) # "{}: {}".format(r.status, r.rc)
+        return "out={}, err={}, rc={}".format(out, err, rc)
     elif request.method == 'GET':
-        if 'id' in request.args:
-            return repr(CONSTRUCTS[request.args['id']])
+        if 'id' in request.args and request.args['id'] in CONSTRUCTS:
+            return {k: v for d in CONSTRUCTS[request.args['id']] for k, v in d.items()}
         else:
             return CONSTRUCTS
